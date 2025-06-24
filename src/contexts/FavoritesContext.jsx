903861879f111dbc35016cc777d8c225608a8d82
michelelongo97 <m.longo7x@gmail.com>
2025-06-24 16:47:27 +0200

    feat: add localStorage to FavoritesContext

--- src/contexts/FavoritesContext.jsx
+++ src/contexts/FavoritesContext.jsx
@@ -1,5 +1,5 @@
 // Importo le funzioni per crare e usare un contesto
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 // Creo il contesto
 const FavoritesContext = createContext();
@@ -7,7 +7,16 @@ const FavoritesContext = createContext();
 // Creo il Provider che fornisce lo stato della ricerca ai componenti figli
 export function FavoritesProvider({ children }) {
   // Creo lo stato per l'aggiunta ai preferiti
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState(() => {
+    // Recupero i dati dal localStorage, se presenti, al primo render, altrimenti inizializzo con un array vuoto
+    const prevState = localStorage.getItem("favorites");
+    return prevState ? JSON.parse(prevState) : [];
+  });
+
+  // Effetto che salva ogni modifica alla lista preferiti nel localStorage che si attiva solo quando "favorites" cambia
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     // Rendo disponibile gli stati a tutti i componenenti figli del Provider
