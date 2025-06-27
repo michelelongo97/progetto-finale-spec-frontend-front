aeb3111b29d93770bd66c48de63d5a4aa3a2199f
michelelongo97 <m.longo7x@gmail.com>
2025-06-27 17:28:50 +0200

    feat: add images in FavoritesPage

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
+// Importo le funzioni React per la gestione della chiamata API
+import { useState, useEffect } from "react";
 // Importo il custom hook dal contesto
 import { useFavoritesContext } from "../contexts/FavoritesContext";
 
+// Importo la funzione API per ricevere i dati riguardo la singola destinazione
+import { fetchDestinationsById } from "../api/destinations";
+
 export default function Favorites() {
   const { favorites, setFavorites } = useFavoritesContext();
+  const [detailedFavorites, setDetailedFavorites] = useState([]);
+
+  useEffect(() => {
+    // Recupera i dettagli solo se ci sono destinazioni da confrontare
+    if (favorites.length === 0) {
+      setDetailedFavorites([]);
+      return;
+    }
+
+    // Effettua una chiamata per ogni destinazione
+    Promise.all(
+      favorites.map((destination) =>
+        fetchDestinationsById(destination.id).then((res) => res.destination)
+      )
+    )
+      .then(setDetailedFavorites)
+      .catch(() => setDetailedFavorites([]));
+  }, [favorites]);
 
   //Creo la funzione per la rimozione dell'elemento dai preferiti
   const removeFromFavorites = (id) => {
@@ -13,13 +36,22 @@ export default function Favorites() {
   return (
     <div className="container">
       <h1 className="mb-4">Preferiti</h1>
-      {favorites.length === 0 ? (
+      {detailedFavorites.length === 0 ? (
         <p className="text-muted">La lista dei preferiti è vuota</p>
       ) : (
         <div className="row g-4">
-          {favorites.map((destination) => (
+          {detailedFavorites.map((destination) => (
             <div key={destination.id} className="col-md-4">
               <div className="card h-100 shadow-sm">
+                {destination.image && (
+                  <img
+                    src={destination.image}
+                    className="card-img-top"
+                    alt={destination.title}
+                    style={{ objectFit: "cover", height: "200px" }}
+                  />
+                )}
+
                 <div className="card-body d-flex flex-column">
                   <h5 className="card-title">
                     <Link
